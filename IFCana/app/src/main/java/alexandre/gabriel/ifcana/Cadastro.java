32f82c50ebcbac4408def1e52ee4f35632d19591
Gabriel Alexandre <gabrielalexandre100398@gmail.com>
2016-07-06 22:18:41 -0400

    Alterações no layout

--- IFCana/app/src/main/java/alexandre/gabriel/ifcana/Cadastro.java
+++ IFCana/app/src/main/java/alexandre/gabriel/ifcana/Cadastro.java
@@ -3,12 +3,45 @@ package alexandre.gabriel.ifcana;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class Cadastro extends ActionBarActivity{
+
+    private Spinner spinnerEquipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
-    }
+
+        spinnerEquipe = (Spinner) findViewById(R.id.spinequipe);
+
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("Vermelha");
+        stringArrayList.add("Azul");
+        stringArrayList.add("Verde");
+        stringArrayList.add("Amarelo");
+
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, stringArrayList);
+        stringArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerEquipe.setAdapter(stringArrayAdapter);
+        spinnerEquipe.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });}
+
+
 }
