4871c4543e25d77b50347eb940e1ba2aab9c8339
Gabriel Alexandre <gabrielalexandre100398@gmail.com>
2016-07-12 11:24:29 -0400

    Alterações no app

---
+++ IFCana/app/src/main/java/alexandre/gabriel/ifcana/Pontuacao.java
@@ -0,0 +1,19 @@
+package alexandre.gabriel.ifcana;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Gabriel on 08/07/2016.
+ */
+public class Pontuacao extends ActionBarActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstaceState) {
+        super.onCreate(savedInstaceState);
+        setContentView(R.layout.pontuacao);
+
+    }
+    
+}
--- IFCana/app/src/main/java/alexandre/gabriel/ifcana/TelaOpcoes2.java
+++ IFCana/app/src/main/java/alexandre/gabriel/ifcana/TelaOpcoes2.java
@@ -1,9 +1,14 @@
 package alexandre.gabriel.ifcana;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaOpcoes2 extends ActionBarActivity {
+
+    Button pontuacao;
 
 
 
@@ -11,6 +16,19 @@ public class TelaOpcoes2 extends ActionBarActivity {
     protected void onCreate(Bundle savedInstaceState){
         super.onCreate(savedInstaceState);
         setContentView(R.layout.tela_opcoes2);
+
+        pontuacao = (Button) findViewById(R.id.btpontuacao);
+        pontuacao.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Intent trocatela = new
+                Intent(TelaOpcoes2.this, Pontuacao.class);
+                TelaOpcoes2.this.startActivity(trocatela);
+                TelaOpcoes2.this.finish();
+
+
+            }
+        });
 
 
     }
